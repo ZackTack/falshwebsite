a8b39b7181e37886729f4d3f4905d5e5080bd4b8
ZackXu <zack.xu.sino@gmail.com>
2020-05-13 10:15:01 -0400

    change on home func

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask,redirect,url_for,render_template
 
 app = Flask(__name__)
 
-@app.route("/<x>")
-def home(x):
-    return f"Hello!{x}"
+@app.route("/")
+def home():
+    return f"Hello!"
 
 @app.route("/admin")
 def admin():
